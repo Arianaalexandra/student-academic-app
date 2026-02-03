0fe391cec35f5742e17f1633c83af6b7075a2c7f
Ariana Alexandra <profirariana5@gmail.com>
2026-02-03 14:54:03 +0200

    Fix SQLite absolute path for Render

--- database.py
+++ database.py
@@ -1,15 +1,16 @@
 import sqlite3
+import os
 
-DB_NAME = "database.db"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "database.db")
 
 def get_db_connection():
-    conn = sqlite3.connect(DB_NAME)
+    conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     return conn
 
 def init_db():
     conn = get_db_connection()
-
     conn.execute("""
         CREATE TABLE IF NOT EXISTS grades (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -19,7 +20,7 @@ def init_db():
             semester INTEGER NOT NULL
         )
     """)
-
     conn.commit()
     conn.close()
+
 
